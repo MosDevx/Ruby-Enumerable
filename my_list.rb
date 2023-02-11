861d04f7f0dcf652efa81faba5ba83fb4dfaf7ab
shella12 <ayeshaarshad4567@gmail.com>
2023-02-11 13:06:29 +0500

    Add MyList class

---
+++ my_list.rb
@@ -0,0 +1,37 @@
+require './my_enumerable.rb'
+
+class MyList
+	include MyEnumerable
+	# @list = []
+
+	def initialize(alist)
+		@list = alist
+	end
+
+	def display
+		@list.each {|a| puts a}
+		# puts @list
+	end
+
+	def each(&block)
+		if block_given?
+			@list.each(&block)
+		# block.call(@list)
+		# can i use the above but now recieve an argument from the implementationsof any? / all? ... as a pram
+		else
+			to_enum(:each)
+		end
+
+	end
+
+end
+
+mylist = [1,2,3,4,5]
+
+list = MyList.new (mylist)
+
+list.display
+
+list.reverse_em
+
+list.all? {|x| x > 2}
